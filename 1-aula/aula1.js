91ef44239c70ff07e045fd2731418075de31a37b
kchagas94 <kenedychagas@gmail.com>
2025-08-07 19:27:50 -0300

    new-lesson-one

---
+++ 1-aula/aula1.js
@@ -0,0 +1,12 @@
+
+//funções
+
+
+// function soma(number1, number2) {
+//    let resultado = number1 + number2;
+//    return resultado;
+// }
+
+let somando = soma(5, 10);
+console.log(somando);
+
