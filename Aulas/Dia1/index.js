cd2c41bc2af83f8cc53eb4ea2c989059a6520723
kenedy <kenedychagas@gmail.com>
2025-11-13 23:04:08 -0300

    dia 1 js

---
+++ Aulas/Dia1/index.js
@@ -0,0 +1,35 @@
+console.log("Está funcionando!");
+
+let firstName = "João";
+console.log(firstName);
+
+let lastName = "Chagas";
+console.log(lastName);
+
+let number1 = 10;
+let number2 = 5;
+
+let soma = number1 + number2;
+console.log("A soma é: " + soma);
+
+let subtracao = number1 - number2;
+console.log("A subtração é: " +  subtracao);
+
+let multiplicacao = number1 * number2;
+console.log("A multiplicação é: " + multiplicacao); 
+
+let divisao = number1 / number2;
+console.log("A divisão é: "+ divisao);
+
+// --------------------------------------------
+
+function percentage10 (price) {
+    return price - (price * 10 / 100);
+}
+let total = percentage10(200);
+console.log("O preço com 10% de desconto é: " + total);
+
+// --------------------------------------------
+
+
+
